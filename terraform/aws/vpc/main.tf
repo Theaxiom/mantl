9dd1f8a1f2f06f07be2ffa01641f8fe168c7f50e
Tom Hummel <tphummel@gmail.com>
2016-08-12 13:49:01 -0700

    vpc: accept cidr_blocks as a string variable
    otherwise, we can't override the value in terraform versions < 0.7

--- terraform/aws/vpc/main.tf
+++ terraform/aws/vpc/main.tf
@@ -2,11 +2,7 @@ variable "availability_zones"  {
   default = "a,b,c"
 }
 variable "cidr_blocks" {
-  default = {
-    az0 = "10.1.1.0/24"
-    az1 = "10.1.2.0/24"
-    az2 = "10.1.3.0/24"
-  }
+  default = "10.1.1.0/24,10.1.2.0/24,10.1.3.0/24"
 }
 variable "datacenter" {default = "aws"}
 variable "long_name" {default = "mantl"}
@@ -28,7 +24,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "main" {
   vpc_id = "${aws_vpc.main.id}"
   count = "${length(split(",", var.availability_zones))}"
-  cidr_block = "${lookup(var.cidr_blocks, concat("az", count.index))}"
+  cidr_block = "${element(split(",", var.cidr_blocks), count.index)}"
   availability_zone = "${var.region}${element(split(",", var.availability_zones), count.index)}"
   tags {
     Name = "${var.long_name}"
